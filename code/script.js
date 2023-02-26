2cf473dd1ec3b54fac375a81e48f7cfa45b7925a
aldyans <writealdyans@gmail.com>
2023-02-26 15:54:35 +0700

    commit awal

---
+++ code/script.js
@@ -0,0 +1,9 @@
+const button = document.querySelectorAll('input.caption-button');
+
+button.forEach(element => {
+    element.addEventListener('click', function(e) {
+        const target = e.target.parentElement;
+        const targetElement = target.querySelector('.caption');
+        targetElement.classList.toggle('active');
+    });
+});
